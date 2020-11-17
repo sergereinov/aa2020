509f4612b6b2d3b31718135baf6984688eaeb48b
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-11-17 06:29:27 +0300

    Homework #2: View & ViewGroups

--- app/src/main/java/com/github/sergereinov/aa2020/MovieDetailsActivity.kt
+++ app/src/main/java/com/github/sergereinov/aa2020/MovieDetailsActivity.kt
@@ -1,14 +1,11 @@
 package com.github.sergereinov.aa2020
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
+        setContentView(R.layout.activity_movie_details)
     }
 }
