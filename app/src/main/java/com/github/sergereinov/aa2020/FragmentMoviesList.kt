c441b49f61184096723944e1a9436f9be6fe94e5
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-11-28 04:39:05 +0300

    добавление фрагмента MoviesList, разметки fragment_movies_list и ресурсов для этого фрагмента

---
+++ app/src/main/java/com/github/sergereinov/aa2020/FragmentMoviesList.kt
@@ -0,0 +1,43 @@
+package com.github.sergereinov.aa2020
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    private var listener: FragmentClicks? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.filmCard)?.setOnClickListener {
+            listener?.filmCard()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as FragmentClicks?
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface FragmentClicks {
+        fun filmCard()
+    }
+}
