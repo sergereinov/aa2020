65626f6d144b7bf838870652d9ddb31a2b2a3ed0
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-11-28 04:40:18 +0300

    Управление фрагментами из MainActivity

--- app/src/main/java/com/github/sergereinov/aa2020/MainActivity.kt
+++ app/src/main/java/com/github/sergereinov/aa2020/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.github.sergereinov.aa2020
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesDetails.FragmentClicks, FragmentMoviesList.FragmentClicks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .apply {
+                    add(R.id.fragments_container, FragmentMoviesList())
+                    commit()
+                }
+        }
     }
+
+    private fun showMovieDetailsFragment() {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                add(R.id.fragments_container, FragmentMoviesDetails())
+                addToBackStack(null)
+                commit()
+            }
+    }
+
+    override fun backFromMovieDetails() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack() //enqueue async pop from backstack
+        }
+    }
+
+    override fun filmCard() {
+        showMovieDetailsFragment()
+    }
+
 }
