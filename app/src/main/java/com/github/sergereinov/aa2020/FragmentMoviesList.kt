74b5cd533ac8a541f7f247651782e80b1bfe8795
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-12-06 20:16:32 +0300

    Добавил модель данных Movie, адаптер MoviesListAdapter, ViewHolder, подключил адаптер к RecycleView и сделал submitList

--- app/src/main/java/com/github/sergereinov/aa2020/FragmentMoviesList.kt
+++ app/src/main/java/com/github/sergereinov/aa2020/FragmentMoviesList.kt
@@ -7,24 +7,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.github.sergereinov.aa2020.data.models.Movie
+import java.util.*
 
 class FragmentMoviesList : Fragment() {
 
     private var listener: FragmentClicks? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<View>(R.id.filmCard)?.setOnClickListener {
+        val movieClickListener = MoviesListAdapter.ClickListener { _ ->
             listener?.filmCard()
         }
+        val adapter = MoviesListAdapter(movieClickListener)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.movies_list)
+        recyclerView.adapter = adapter
+
+        adapter.submitList(moviesList)
     }
 
     override fun onAttach(context: Context) {
@@ -39,5 +47,54 @@ class FragmentMoviesList : Fragment() {
 
     interface FragmentClicks {
         fun filmCard()
+    }
+
+    companion object {
+        private val moviesList = listOf(
+                Movie(
+                        id = 1,
+                        movieImageId = R.drawable.movie,
+                        pg = "13+",
+                        isLiked = false,
+                        tags = "Action, Adventure, Drama",
+                        starsCount = 4,
+                        reviews = "125 Reviews",
+                        title = "Avengers: End Game",
+                        movieLen = "137 min"
+                ),
+                Movie(
+                        id = 2,
+                        movieImageId = R.drawable.movie2,
+                        pg = "16+",
+                        isLiked = true,
+                        tags = "Action, Sci-Fi, Thriller",
+                        starsCount = 5,
+                        reviews = "98 Reviews",
+                        title = "Tenet",
+                        movieLen = "97 min"
+                ),
+                Movie(
+                        id = 3,
+                        movieImageId = R.drawable.movie3,
+                        pg = "13+",
+                        isLiked = false,
+                        tags = "Action, Adventure, Sci-Fi",
+                        starsCount = 4,
+                        reviews = "38 Reviews",
+                        title = "Black Widow",
+                        movieLen = "102 min"
+                ),
+                Movie(
+                        id = 4,
+                        movieImageId = R.drawable.movie4,
+                        pg = "13+",
+                        isLiked = false,
+                        tags = "Action, Adventure, Fantasy",
+                        starsCount = 5,
+                        reviews = "74 Reviews",
+                        title = "Wonder Woman 1984",
+                        movieLen = "120 min"
+                )
+        )
     }
 }
