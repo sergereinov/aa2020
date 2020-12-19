301cab4ef13aaee5fcaae004b695d621afd26b5e
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-12-19 05:23:58 +0300

    импорт подготовленных assets и data

---
+++ app/src/main/java/com/github/sergereinov/aa2020/data/Actor.kt
@@ -0,0 +1,7 @@
+package com.github.sergereinov.aa2020.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ app/src/main/java/com/github/sergereinov/aa2020/data/Genre.kt
@@ -0,0 +1,3 @@
+package com.github.sergereinov.aa2020.data
+
+data class Genre(val id: Int, val name: String)
