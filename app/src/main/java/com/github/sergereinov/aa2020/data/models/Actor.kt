e7c883400a8ca0b35572f73d8640216bc237b957
sergereinov <47950629+sergereinov@users.noreply.github.com>
2020-12-19 05:32:09 +0300

    удаление устаревших файлов

--- app/src/main/java/com/github/sergereinov/aa2020/data/models/Actor.kt
+++
@@ -1,7 +0,0 @@
-package com.github.sergereinov.aa2020.data.models
-
-data class Actor(
-    val id: Int,
-    val imageId: Int,
-    val name: String
-)
--- app/src/main/java/com/github/sergereinov/aa2020/data/models/MovieDetails.kt
+++
@@ -1,5 +0,0 @@
-package com.github.sergereinov.aa2020.data.models
-
-data class MovieDetails(
-        val actors: List<Actor>
-)
